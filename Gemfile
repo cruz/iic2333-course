80b18ddbaeddd94929d2e89aeb91344cacaa714b
Dietrich Daroch <dietr1ch@acm.org>
2015-12-17 15:51:59 -0300

    [Init] Bare middleman+revealjs+slim project

---
+++ Gemfile
@@ -0,0 +1,17 @@
+# If you do not have OpenSSL installed, update
+# the following line to use 'http://' instead
+source 'https://rubygems.org'
+
+gem 'slim', '>= 3.0'
+
+# For faster file watcher updates on Windows:
+gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
+
+# windows does not come with time zone data
+gem 'tzinfo-data', platforms: [:mswin, :mingw]
+
+# Middleman Gems
+gem 'middleman-sprockets', '>= 4.0.0.rc.1'
+gem 'middleman-compass', '>= 4.0.0.rc.1'
+gem 'middleman', '>= 4.0.0.rc.1'
+gem 'middleman-livereload'
---
+++ config.rb
@@ -0,0 +1,60 @@
+require 'slim'
+
+set :site_title, "IIC2333 - SO y Redes"
+#set :site_url, "http://www.domain.com"
+#set :site_description, "Meta description."
+#set :site_keywords, "keyword-one, keyword-two"
+
+# Page options, layouts, aliases and proxies
+# ============
+set :relative_links, true
+
+
+# Assets
+# ======
+set :css_dir,    "css"
+set :js_dir,     "js"
+set :images_dir, "images"
+
+
+# Per-page layout changes
+# -----------------------
+# With no layout
+page '/*.xml',  layout: false
+page '/*.json', layout: false
+page '/*.txt',  layout: false
+
+# With alternative layout
+# page '/path/to/file.html', layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy '/this-page-has-no-template.html', '/template-file.html', locals: {
+#  which_fake_page: 'Rendering a fake page with a local variable' }
+
+
+
+# Helpers
+# =======
+
+# Reload the browser automatically whenever files change
+# configure :development do
+#   activate :livereload
+# end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     'Helping'
+#   end
+# end
+
+
+# Build-specific configuration
+# ============================
+configure :build do
+  # activate :minify_css
+  # activate :minify_javascript
+  # activate :minify_html
+
+  # activate :asset_hash
+end
