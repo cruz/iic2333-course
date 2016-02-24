6ba96b0f46bfb28c6d47c2017c65a4b6aebc40b3
Dietrich Daroch <dietr1ch@acm.org>
2016-02-24 16:07:46 -0300

    [WIP] Add Slides 2-3

---
+++ source/activities/1-C/type_sizes.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+int main() {
+  printf("         char: %d\n", sizeof(         char));
+  printf("    short int: %d\n", sizeof(    short int));
+  printf("          int: %d\n", sizeof(          int));
+  printf("     long int: %d\n", sizeof(     long int));
+  printf("long long int: %d\n", sizeof(long long int));
+  printf("        float: %d\n", sizeof(        float));
+  printf("       double: %d\n", sizeof(       double));
+}
