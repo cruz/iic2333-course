e4f7d64a6080420738ad8ed1404e5dff7babdf82
IIC2233 Team <cruz@ing.puc.cl>
2020-03-16 10:01:49 -0300

    Remove 2019-2 materials

--- source/homeworks/P2/ejemplo-cliente-servidor/client/src/comunication.h
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
-
-void client_send_message(int client_socket, int pkg_id, char * message);
-int client_receive_id(int client_socket);
-char * client_receive_payload(int client_socket);
